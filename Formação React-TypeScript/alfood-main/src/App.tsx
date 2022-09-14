fc6ba27888697760f5e11b8328b9786b4ccf2e52
Matheus-Farah <matheusfarah00@gmail.com>
2022-09-14 17:44:11 -0300

    feat: inicio de outro projeto

---
+++ Formação React-TypeScript/alfood-main/src/App.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import VitrineRestaurantes from "./pages/VitrineRestaurantes";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/restaurantes" element={<VitrineRestaurantes />} />
+    </Routes>
+  );
+}
+
+export default App;
