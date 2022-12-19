c1a86c280b61ee686fb4318a86e25680f8ace52e
Matheus Farah <matheusfarah00@gmail.com>
2022-12-19 17:30:05 -0300

    Atualização do projeto

--- Formacao Angular/gatitobook/src/app/autenticacao/autenticacao.service.ts
+++ Formacao Angular/gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -1,27 +1,35 @@
+import { environment } from './../../environments/environment';
 import { UsuarioService } from './usuario/usuario.service';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs';
 
+const API = environment.apiURL;
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AutenticacaoService {
-
-  constructor(private httpClient: HttpClient, private usuarioService: UsuarioService) { }
+  constructor(
+    private httpClient: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
 
   autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
-    return this.httpClient.post('http://localhost:3000/user/login', {
-      userName: usuario,
-      password: senha,
-    },
-      { observe: 'response' }
-    ).pipe(
-      tap((res) => {
-        const authToken = res.headers.get('x-access-token') ?? '';
-        this.usuarioService.salvaToken(authToken);
-      })
-    )
+    return this.httpClient
+      .post(
+        `${API}/user/login`,
+        {
+          userName: usuario,
+          password: senha,
+        },
+        { observe: 'response' }
+      )
+      .pipe(
+        tap((res) => {
+          const authToken = res.headers.get('x-access-token') ?? '';
+          this.usuarioService.salvaToken(authToken);
+        })
+      );
   }
 }
