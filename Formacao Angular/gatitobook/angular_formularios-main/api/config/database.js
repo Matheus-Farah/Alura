21bd6fa4babe597ed5dd27464c5447f991181ce3
Matheus Farah <matheusfarah00@gmail.com>
2022-12-21 17:54:41 -0300

    Criação da página de animal, funções curtit e deletar, correção de código

--- Formacao Angular/gatitobook/angular_formularios-main/api/config/database.js
+++ Formacao Angular/gatitobook/angular_formularios-main/api/config/database.js
@@ -21,15 +21,6 @@ INSERT INTO user (
 ) SELECT 'alvaro', 'alvaro@gatitobook.com.br', '12345678', 'Alvaro' WHERE NOT EXISTS (SELECT * FROM user WHERE user_name = 'alvaro')
 `;
 
-const INSERT_DEFAULT_USER_2 = `
-INSERT INTO user (
-    user_name,
-    user_email,
-    user_password,
-    user_full_name
-) SELECT 'math', 'matheus@email.com', '554433', 'Matheus' WHERE NOT EXISTS (SELECT * FROM user WHERE user_name = 'matheus')
-`;
-
 const PHOTO_SCHEMA = `
 CREATE TABLE IF NOT EXISTS photo (
     photo_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -70,7 +61,7 @@ CREATE TABLE IF NOT EXISTS like (
 db.serialize(() => {
   db.run("PRAGMA foreign_keys=ON");
   db.run(USER_SCHEMA);
-  db.run(INSERT_DEFAULT_USER_2);
+  db.run(INSERT_DEFAULT_USER_1);
   db.run(PHOTO_SCHEMA);
   db.run(COMMENT_SCHEMA);
   db.run(LIKE_SCHEMA);
